bf5dff59d3c32249f6a5c415d2a994629f8e1e95
Minh Truong <61044654+biradon@users.noreply.github.com>
2023-11-02 23:31:15 -0400

    Add Google Map widget
    and activate API KEY from Google Cloud Platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'widgets/AppTitle.dart';
 import 'widgets/InputField.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,20 +28,31 @@ class MyApp extends StatelessWidget {
                 hintText: 'Select distance for the alarm',
               ),
               ElevatedButton(
-                  onPressed: (){
-                    // Logic go here later
-                    // When press the button will be send the API to
-                    // Google API which location and draw the radius around
-                  },
-              style: ElevatedButton.styleFrom(
-                primary: Colors.lightBlueAccent, // Background color of the button
-                textStyle: TextStyle(fontSize: 20), // Text style
-                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10), // Border radius
-                ),
+                onPressed: () {
+                  // Logic go here later
+                  // When pressed, send the API to Google API which gets location and draws the radius around
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.lightBlueAccent, // Background color of the button
+                  textStyle: TextStyle(fontSize: 20), // Text style
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), // Border radius
                   ),
-              child: Text('OK'))
+                ),
+                child: Text('OK'),
+              ),
+              Expanded(
+                child: GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(37.7749, -122.4194), // Initial map location
+                    zoom: 15.0, // Initial zoom level
+                  ),
+                  onMapCreated: (GoogleMapController controller) {
+                    // Add your logic after the map is created
+                  },
+                ),
+              ),
             ],
           ),
         ),
@@ -48,4 +60,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
